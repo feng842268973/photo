57023876788da20dccfbfbac38299053084af683
wangchaofeng <feng842268973@qq.com>
2020-12-23 18:24:03 +0800

    出书画

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="getToken">gettoken</button>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+    getToken(){
+      this.$axios.get(
+        '/oauth/2.0/token',
+        {params:{
+          grant_type:'client_credentials',
+          client_id:'XdWbn0drKXZopGEEBbj409PU',
+          client_secret:'x9txZVgLzW45MYvRYpknBkopKvDhRRD5'
+        }}
+      ).then(res=>{
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="img" src="../../images/test.jpg" alt="">
+   <button @click="getimage">getimage</button>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      
+    }
+  },
+  computed:{
+    img(){
+      return this.getBase64Image(document.querySelector('.img'))
+    }
+  },
+  methods:{
+    getBase64Image(img) {  
+      var canvas = document.createElement("canvas");  
+      canvas.width = img.width/2;  
+      canvas.height = img.height/2;  
+      var ctx = canvas.getContext("2d");  
+      ctx.drawImage(img, 0, 0, img.width, img.height);  
+      var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();  
+      var dataURL = canvas.toDataURL("image/"+ext);  
+      return dataURL.slice(22);  
+  } ,
+    getimage(){
+    //access_token  
+
+      let tk=  '24.ae176569874e3d14813a532449f936d5.2592000.1611307231.282335-23131836'
+      let rectangle=[{'width': 200, 'top': 0, 'height': 345, 'left': 0}]
+      this.$axios.post(
+        '/rest/2.0/image-process/v1/inpainting?access_token='+tk,
+        {
+          rectangle:rectangle,
+          // image:this.getBase64Image(this.img)
+          image:this.img
+        }
+      ).then(res=>{
+        console.log(res)
+      })
+    }
   }
 }
 </script>
