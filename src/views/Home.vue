05be01c452339aba5a91914ecf3ab5bbf555c78a
wangchaofeng <feng842268973@qq.com>
2020-12-25 18:44:59 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img class="img" src="../../images/test.jpg" alt="">
+    <img class="img" src="../../images/test2.jpg" alt="">
    <button @click="getimage">getimage</button>
     
   </div>
@@ -13,7 +13,7 @@ export default {
   name: 'Home',
   data(){
     return {
-      
+      prefixImg:''
     }
   },
   computed:{
@@ -24,19 +24,22 @@ export default {
   methods:{
     getBase64Image(img) {  
       var canvas = document.createElement("canvas");  
-      canvas.width = img.width/2;  
-      canvas.height = img.height/2;  
+      canvas.width = img.width;  
+      canvas.height = img.height;  
       var ctx = canvas.getContext("2d");  
       ctx.drawImage(img, 0, 0, img.width, img.height);  
       var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();  
-      var dataURL = canvas.toDataURL("image/"+ext);  
-      return dataURL.slice(22);  
+      var dataURL = canvas.toDataURL("image/"+ext); 
+      let _pre=/data\:image\/(jpg|jpeg|png)\;base64\,/.exec(dataURL)
+      console.log(_pre)
+      this.prefixImg=_pre[0]
+      return dataURL.replace(_pre[0],'');  
   } ,
     getimage(){
     //access_token  
 
       let tk=  '24.ae176569874e3d14813a532449f936d5.2592000.1611307231.282335-23131836'
-      let rectangle=[{'width': 200, 'top': 0, 'height': 345, 'left': 0}]
+      let rectangle=[{'width': 57, 'top': 174, 'height': 100, 'left': 92}]
       this.$axios.post(
         '/rest/2.0/image-process/v1/inpainting?access_token='+tk,
         {
